816044a638291cf605991ec3c4fb566a6871f840
supalapka <yurabalint0103@gmail.com>
2020-11-17 12:54:56 +0200

    update basesqloperations class

--- app/db/BaseSQLOperations.php
+++ app/db/BaseSQLOperations.php
@@ -10,6 +10,12 @@ class BaseSQLOperations implements IBaseSQLOperations
 {
     public static function select(DbConnection $connection, string $table, string $condition = null, string $classname = null)
     {
+        
+        if (!$connection) {
+            $connection = new DbConnection(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+            //die("Connection failed: " . mysqli_connect_error());
+          }
+
         $query = $condition ? "SELECT * FROM $table WHERE $condition" : "SELECT * FROM $table";
         $data = $connection->query($query)->fetch_all(MYSQLI_ASSOC);
         if ($classname != null) {
@@ -23,15 +29,44 @@ class BaseSQLOperations implements IBaseSQLOperations
                 return $result;
             }
         }
+        $connection->close();
         return $data;
     }
     public static function insert(DbConnection $connection, string $table, array $values, array $columns = null)
     {
+        if (!$connection) {
+            $connection = new DbConnection(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+            //die("Connection failed: " . mysqli_connect_error());
+          }
+
+             //TODO
+
+          $connection->close();
     }
     public static function update(DbConnection $connection, string $table, array $columns, array $values, string $condition)
     {
+
+        if (!$connection) {
+            $connection = new DbConnection(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+            //die("Connection failed: " . mysqli_connect_error());
+          }
+
+            //TODO
+
+
+          $connection->close();
     }
     public static function delete(DbConnection $connection, string $table, string $condition = null)
     {
+
+        if (!$connection) {
+            $connection = new DbConnection(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+            //die("Connection failed: " . mysqli_connect_error());
+          }
+
+   //TODO
+
+
+          $connection->close();
     }
 }
