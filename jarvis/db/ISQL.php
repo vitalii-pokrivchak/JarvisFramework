f655f7814b1151b14a1e3f03299d41e01aaf0dfb
vitalii-pokrivchak <pokrivchakzvitalja@gmail.com>
2020-11-23 00:05:43 +0200

    update Bundle Collection and move dbcore to the PDO

--- jarvis/db/ISQL.php
+++ jarvis/db/ISQL.php
@@ -4,9 +4,9 @@ namespace jarvis\db;
 
 use jarvis\models\ModelObject;
 
-interface IBaseSQLOperations
+interface ISQL
 {
-    public static function select(string $table, string $condition = null, string $classname = null);
+    public static function select(string $table, string $condition = null, string $classname = null): ?array;
     public static function insert(string $table, ModelObject $model);
     public static function update(string $table, ModelObject $model, string $condition);
     public static function delete(string $table, string $condition = null);
--- jarvis/models/IModel.php
+++ jarvis/models/IModel.php
@@ -4,8 +4,8 @@ namespace jarvis\models;
 
 interface IModel
 {
-    public function get_all();
-    public function get(int $id);
+    public function get_all(): array;
+    public function get(int $id): ModelObject;
     public function write();
     public function update();
     public function delete();
