59c213c35691a16219ed7c38ee36ceb401b2d684
vitalii-pokrivchak <pokrivchakzvitalja@gmail.com>
2020-12-10 23:56:03 +0200

    clear project and add todo

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,6 @@
 require_once 'vendor/autoload.php';
 
 use jarvis\core\Application;
-use jarvis\core\ConfigurationManager;
 
 ini_set('display_errors', 'on');
 ini_set('display_startup_errors', 'on');
